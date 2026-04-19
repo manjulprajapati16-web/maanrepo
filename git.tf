3dd8836218dabc2064dce0bec0b63af38bcf535f
manju <manjulprajapati16may@gmail.com>
2026-04-19 20:23:14 +0530

    first lesson

---
+++ git.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.69.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features{}
+}
+
+
+resource "azurerm_resource_group" "lesson-rg" {
+  name     = "lesson-rg"
+  location = "West Europe"
+}
