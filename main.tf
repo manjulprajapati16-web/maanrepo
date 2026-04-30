760b130473293a181fb169ada3bbafb60c4f77d5
manju <manjulprajapati16may@gmail.com>
2026-04-30 15:49:17 +0530

    second edit

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "companyrg" {
   name     = "companyrg"
   location = "East us"
 }
+resource "azurerm_resource_group" "company2" {
+  name = "companyrg2"
+  location = "central india"
+}
