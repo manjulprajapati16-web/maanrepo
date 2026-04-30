5fba9556d21bd7d8af214515f013e2f8e6ecd504
manju <manjulprajapati16may@gmail.com>
2026-04-30 15:59:00 +0530

    third edit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "hafsa dii" {
   name     = "hafsa dii"
   location = "central india"
 }
-resource "azurerm_resource_group" "maan" {
-  name = "maan"
-  location = "west us"
+resource "azurerm_resource_group" "maan16" {
+  name = "maan16"
+  location = "south india"
 }
