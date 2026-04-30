c711007e6bd531f74cfd4f933079f3e4eb220de3
manju <manjulprajapati16may@gmail.com>
2026-04-30 16:15:10 +0530

    push2

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "manjulrg" {
   name = "manjul"
   location = "central india"
 }
+resource "azurerm_resource_group" "yashfarg" {
+    name = "yashfarg"
+    location = "south india"
+  
+}
