8e11bad6c19ac32efa24897ec8f24f827f2f1d63
manju <manjulprajapati16may@gmail.com>
2026-04-30 15:54:00 +0530

    fourth commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "company1" {
   name     = "company1"
   location = "west us"
 }
-resource "azurerm_resource_group" "company2" {
-  name = "companyrg2"
-  location = "central india"
+resource "azurerm_resource_group" "company" {
+  name = "company"
+  location = "east us"
 }
